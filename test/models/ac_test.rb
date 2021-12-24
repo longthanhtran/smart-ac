bfa86cf85379b52af7a554f76f4c55192e733ae4
Long Tran <long.thanh.tran@gmail.com>
2021-12-24 17:33:41 +0700

    provide unit test for ac model

--- test/models/ac_test.rb
+++ test/models/ac_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class AcTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @valid_ac = build(:ac).serializable_hash
+    @invalid_ac = build(:ac, serial_number: nil)
+  end
+
+  test "a valid ac can be saved" do
+    ac = Ac.new(@valid_ac)
+    assert ac.valid?
+    assert_not_nil ac.save
+  end
+
+  test "can not save invalid ac" do
+  end
 end
