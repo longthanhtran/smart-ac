b59b2b3c021899eb92c4c041ef842ec15aadc933
Long TRAN <longthanhtran@users.noreply.github.com>
2021-12-30 16:50:23 +0700

    Closed #8 (#20)

--- app/controllers/acs_controller.rb
+++ app/controllers/acs_controller.rb
@@ -1,71 +1,20 @@
 class AcsController < ApplicationController
   before_action :require_user_logged_in!
-  before_action :set_ac, only: %i[ show edit update destroy ]
 
-  # GET /acs or /acs.json
   def index
     @acs = Ac.all
   end
 
-  # GET /acs/1 or /acs/1.json
   def show
+    @ac = Ac.find(params[:id]); p @ac
   end
 
-  # GET /acs/new
-  def new
-    @ac = Ac.new
-  end
+  def show_by_sn
+    @ac = Ac.find_by(serial_number: params[:serial_number])
 
-  # GET /acs/1/edit
-  def edit
-  end
-
-  # POST /acs or /acs.json
-  def create
-    @ac = Ac.new(ac_params)
-
-    respond_to do |format|
-      if @ac.save
-        format.html { redirect_to ac_url(@ac), notice: "Ac was successfully created." }
-        format.json { render :show, status: :created, location: @ac }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @ac.errors, status: :unprocessable_entity }
-      end
+    if ! @ac
+      flash[:notice] = "Can not find the ac with #{params[:serial_number]}"
     end
   end
 
-  # PATCH/PUT /acs/1 or /acs/1.json
-  def update
-    respond_to do |format|
-      if @ac.update(ac_params)
-        format.html { redirect_to ac_url(@ac), notice: "Ac was successfully updated." }
-        format.json { render :show, status: :ok, location: @ac }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @ac.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /acs/1 or /acs/1.json
-  def destroy
-    @ac.destroy
-
-    respond_to do |format|
-      format.html { redirect_to acs_url, notice: "Ac was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_ac
-      @ac = Ac.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def ac_params
-      params.require(:ac).permit(:serial_number, :registration_date, :firmware_version)
-    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 
 User.create(email: 'long@example.com', password: 's3cret!22', password_confirmation: 's3cret!22')
 User.create(email: 'long.tran@example.com', password: 's3cret!22', password_confirmation: 's3cret!22')
+
+Ac.create(serial_number: 'DAI123', registration_date: '2021-12-30', firmware_version: 'DAIA001')
+Ac.create(serial_number: 'CSP123', registration_date: '2021-12-27', firmware_version: 'CSPA001')
